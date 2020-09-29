e34516c3f6e93c92feff2e5a7413e6c4b848b57e
andimeon <andimeon@gmail.com>
2020-09-29 22:14:43 +0900

    Add customer coordinates

--- distance.py
+++ distance.py
@@ -9,46 +9,16 @@ def get_min_distance(lon, lat, pizzerias):
         user_distances.append({
             'name': pizzeria['alias'],
             'address': pizzeria['address'],
-            'distance': distance.distance(pizzeria_coordinate, (lat, lon)).km
+            'lat': float(pizzeria['lat']),
+            'lon': float(pizzeria['lon']),
+            'client_lat': lat,
+            'client_lon': lon,
+            'distance': distance.distance(pizzeria_coordinate, (lat, lon)).km,
+            'deliveryman': pizzeria['deliveryman']
         })
-    
     closest_pizzeria = min(user_distances, key=get_user_distance)
-    reply_text = create_distance_reply(closest_pizzeria)
-    
-    return reply_text
+
+    return closest_pizzeria
 
 def get_user_distance(user_distances):
     return user_distances['distance']
-
-
-def create_distance_reply(pizzeria):
-    address = pizzeria['address']
-    if pizzeria['distance'] <= 0.5:
-        distance = int(pizzeria['distance'] * 1000)
-        return dedent(f'''
-        Может заберёте пиццу из нашей пиццерии неподалеку? 
-        Она всего в {distance} метров от Вас! Вот ее адрес: {address}. 
-        Но можем доставить и бесплатно! Нам не сложно)''')
-
-    elif pizzeria['distance'] <= 5:
-        return dedent('''
-        Похоже придется ехать до Вас на самокате. 
-        Доставка будет стоить 100 руб. 
-        Доставляем или самовывоз?''')
-
-    elif pizzeria['distance'] <= 20:
-        return dedent(f'''
-        Вы довольно далеко от нас. Ближайшая к вам пиццерия
-        находится по адресу: {address}. Доставка будет стоить 300 руб.
-        Но Вы можете забрать пиццу самостоятельно)
-        ''')
-    else:
-        distance = int(pizzeria['distance'])
-        return dedent(f'''
-        К сожалению Вы находитесь далеко от нас,
-        Ближайшая пиццерия аж в {distance} км от Вас!
-        ''')
-
-
-if __name__ == "__main__":
-    pass
