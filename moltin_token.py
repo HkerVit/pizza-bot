1b38539b9634cacc59aad01ad3f4de4455b7c2b9
andimeon <andimeon@gmail.com>
2020-10-01 13:32:20 +0900

    Rename to moltin_token.py

---
+++ moltin_token.py
@@ -0,0 +1,27 @@
+from environs import Env
+import time
+import json
+import requests
+
+env = Env()
+env.read_env()
+
+client_id = env('CLIENT_ID')
+client_secret = env('CLIENT_SECRET_TOKEN')
+
+
+def get_access_token():
+    data = {
+        'client_id': client_id,
+        'client_secret': client_secret,
+        'grant_type': 'client_credentials'
+    }
+
+    url = 'https://api.moltin.com/oauth/access_token'
+
+    response = requests.post(url, data=data)
+    response.raise_for_status()
+    access_response = response.json()
+
+    return access_response['access_token'], access_response['expires']
+
