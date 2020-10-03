296e9092d6dbad2863e48bc994a2081771f4f504
andimeon <andimeon@gmail.com>
2020-10-04 08:54:14 +0900

    Update callback_data

--- keyboard.py
+++ keyboard.py
@@ -23,12 +23,12 @@ def get_menu_keyboard(products, menu_page_number):
     return InlineKeyboardMarkup(products_keyboard)
 
 
-def get_product_keyboard(products, product_id, token):
+def get_product_keyboard_and_text(products, product_id, token):
     product = next((product for product in products if product['id'] == product_id))
     image = moltin_bot_function.get_image_url(token, product['image_id'])
 
     product_keyboard = [
-        [InlineKeyboardButton(f'Выбрать - {product["name"]}', callback_data=f'{product_index}')],
+        [InlineKeyboardButton(f'Выбрать - {product["name"]}', callback_data=f'{product_id}')],
         [InlineKeyboardButton('Меню', callback_data='menu')],
         ]
     reply_markup = InlineKeyboardMarkup(product_keyboard)
@@ -39,3 +39,27 @@ def get_product_keyboard(products, product_id, token):
     ''')
 
     return reply_markup, message, image
+
+
+def get_cart_keyboard_and_text(token, chat_id):
+    cart, total_amount = moltin_bot_function.get_cart_items(token, chat_id)
+    message = ''
+    cart_keyboard = []
+    for product in cart:
+        cart_keyboard.append([InlineKeyboardButton(f"Убрать из корзины {product['name']}", callback_data=f"remove,{product['id']}")])
+
+        product_output = dedent(f'''
+            Пицца {product['name']}
+            {product['description']}
+            По цене {product['price']} руб
+
+            В заказе {product['quantity']} за {product['amount']} руб
+            ''')
+        message += product_output
+
+    cart_keyboard.append([InlineKeyboardButton('Меню', callback_data='menu')])
+    cart_keyboard.append([InlineKeyboardButton('Выбор доставки', callback_data='delivery_choice')])
+    reply_markup = InlineKeyboardMarkup(cart_keyboard)
+    message += f'\nВсего к оплате: {total_amount} руб'
+
+    return reply_markup, message, cart, total_amount
