9ce6b66b41d9169d4aa1ce43547eddaa4b31af49
andimeon <andimeon@gmail.com>
2020-10-23 22:51:07 +0900

    Add handle unknown message

--- keyboard.py
+++ keyboard.py
@@ -14,8 +14,11 @@ def get_menu_keyboard(chat_id, products, menu_button):
     if menu_button == '/start' or menu_button == 'menu':
         page_number = 1
     else:
-        __, page_number = menu_button.split(',')
-        page_number = int(page_number)
+        try:
+            __, page_number = menu_button.split(',')
+            page_number = int(page_number)
+        except ValueError:
+            return None
     
     products_keyboard = [
             [InlineKeyboardButton(product['name'], callback_data=product['id'])] 
