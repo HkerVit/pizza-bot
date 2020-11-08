4b3ff1bbb8e3183c6b6299667e1f9328a4c70fdb
andimeon <andimeon@gmail.com>
2020-11-08 23:48:17 +0900

    Add menu handler

--- app.py
+++ app.py
@@ -7,7 +7,9 @@ from flask import Flask, request, send_file
 import redis
 from environs import Env
 
-import fb_menu_keyboard, fb_help_keyboard
+import fb_menu_keyboard
+import fb_help_message
+import fb_add_to_cart_message
 from moltin_token import get_token
 
 app = Flask(__name__)
@@ -19,23 +21,21 @@ env = Env()
 env.read_env()
 
 
-@app.route('/', methods=['GET'])
-def verify():
-    if request.args.get('hub.mode') == 'subscribe' and request.args.get('hub.challenge'):
-        if not request.args.get('hub.verify_token') == env('VERIFY_TOKEN'):
-            return 'Verification token mismatch', 403
-        return request.args['hub.challenge'], 200
-    return 'Hello world', 200
-
-
-def handle_start(sender_id, message):
-    fb_menu_keyboard.send_menu(sender_id, moltin_token, message)
-    return 'START'
+def handle_start(sender_id, message, db):
+    fb_menu_keyboard.send_menu(sender_id, moltin_token, db, message)
+    return 'MENU'
 
 
 def handle_help(sender_id, message):
-    fb_help_keyboard.send_help_message(sender_id, message)
+    fb_help_message.send_help_message(sender_id, message)
     return 'START'
+
+
+def handle_menu(sender_id, message, db):
+    if 'add_to_cart' in message:
+        fb_add_to_cart_message.send_add_to_cart_message(sender_id, message, moltin_token, db)
+
+    return 'MENU'
 
 
 def handle_users_reply(sender_id, message_text):
@@ -47,26 +47,39 @@ def handle_users_reply(sender_id, message_text):
     states_functions = {
         'START': handle_start,
         'HELP': handle_help,
+        'MENU': handle_menu,
     }
 
     user = f'fb_{sender_id}'
     recorded_state = db.get(user)
     if not recorded_state or recorded_state.decode('utf-8') not in states_functions.keys():
-        user_state = 'HELP'
-    else:
-        user_state = recorded_state.decode('utf-8')
-    if message_text == '/start' or message_text == 'menu':
-        user_state = 'START'
-    elif 'start' in message_text:
         user_state = 'START'
     else:
-        user_state = 'HELP'
+        if message_text == '/start' or message_text == 'menu':
+            user_state = 'START'
+        elif 'start' in message_text:
+            user_state = 'START'
+        else:
+            user_state = recorded_state.decode('utf-8')
+    
+    if user_state is None:
+        user_state == 'HELP'
+        
     state_handler = states_functions[user_state]
     try:
-        next_state = state_handler(sender_id, message_text)
+        next_state = state_handler(sender_id, message_text, db)
         db.set(user, next_state)
     except Exception as err:
         logging.exception(err)
+
+
+@app.route('/', methods=['GET'])
+def verify():
+    if request.args.get('hub.mode') == 'subscribe' and request.args.get('hub.challenge'):
+        if not request.args.get('hub.verify_token') == env('VERIFY_TOKEN'):
+            return 'Verification token mismatch', 403
+        return request.args['hub.challenge'], 200
+    return 'Hello world', 200
 
 
 @app.route('/', methods=['POST'])
@@ -77,7 +90,6 @@ def webhook():
             for messaging_event in entry['messaging']:
                 if messaging_event.get('message'):
                     sender_id = messaging_event['sender']['id']
-                    # recipient_id = messaging_event["recipient"]["id"]
                     message = messaging_event['message']['text']
                     handle_users_reply(sender_id, message)
                 elif messaging_event.get('postback'):
@@ -93,6 +105,8 @@ def get_image():
         filename = 'img/pizza_logo.png'
     elif request.args.get('type') == '2':
         filename = 'img/pizza_category.jpg'
+    elif request.args.get('type') == '3':
+        filename = 'img/cart.jpg'
     else:
         return "Bad request", 400
 
